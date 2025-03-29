5502d1744871b3c3818d8405f508027c28520494
thakun399 <thakun.niam@bumail.net>
2025-03-29 15:11:48 +0700

    PkUNGtELLJUSTUP

--- Assets/MYYYYYYYYYYYYYYY/Scr/Bullet.cs
+++ Assets/MYYYYYYYYYYYYYYY/Scr/Bullet.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public ParticleSystem explosionParticle;
+    public float damage = 100f; // ดาเมจของกระสุน
+    public float lifeTime = 5f; // กระสุนจะหายไปหลังจาก 5 วินาที
+
     void Start()
     {
-        
+        Destroy(gameObject, lifeTime); // ทำลายกระสุนถ้าไม่ได้โดนอะไร
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        // เช็กว่ากระสุนชนกับตึกหรือไม่
+        if (other.CompareTag("Building"))   
+        {
+            Debug.Log("Hit");
+            BuildingHealth building = other.GetComponent<BuildingHealth>();
+            if (building != null)
+            {
+                building.TakeDamage(damage); // ลดเลือดตึก
+            }
+
+            Instantiate(explosionParticle, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject); // ทำลายกระสุนเมื่อชนตึก
+        }
     }
+
+
 }
